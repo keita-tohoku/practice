8e39ece337529b03e0f072df051ea98554e4a9f6
keita-tohoku <keita.hirohara.q5@dc.tohoku.ac.jp>
2017-11-08 19:56:12 +0900

    Add files via upload

---
+++ MissingInteger.c
@@ -0,0 +1,19 @@
+int solution(int A[], int N) {
+    // write your code in C99 (gcc 6.2.0)
+    int *B = (int *)calloc(N,sizeof(int));
+    
+    for (int i =0; i<N; i++) {
+        if(A[i] > 0 && A[i] < N+1) {
+            B[A[i]-1] = 1;
+        }
+    }
+    
+    for (int i=0; i<N;i++) {
+        if (B[i] == 0) {
+            return i+1;
+        }
+    }
+    
+    return N+1;
+    
+}
